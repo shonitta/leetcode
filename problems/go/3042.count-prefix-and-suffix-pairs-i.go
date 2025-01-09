07cc0f7a4cdd455c9298ac68e884da637615fd45
shonitta <shonitta.524@gmail.com>
2025-01-09 09:34:05 +0900

    [skip ci] Fix variable names

--- problems/go/3042.count-prefix-and-suffix-pairs-i.go
+++ problems/go/3042.count-prefix-and-suffix-pairs-i.go
@@ -13,9 +13,9 @@ func countPrefixSuffixPairs(words []string) int {
 	count := 0
 	for i := 0; i < len(words); i++ {
 		for j := i + 1; j < len(words); j++ {
-			has_prefix := strings.HasPrefix(words[j], words[i])
-			has_suffix := strings.HasSuffix(words[j], words[i])
-			if has_prefix && has_suffix {
+			hasPrefix := strings.HasPrefix(words[j], words[i])
+			hasSuffix := strings.HasSuffix(words[j], words[i])
+			if hasPrefix && hasSuffix {
 				count++
 			}
 		}
